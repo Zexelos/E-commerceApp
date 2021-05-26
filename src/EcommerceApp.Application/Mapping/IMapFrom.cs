c9cb51e97c330acb09f0adef98aed6e4e7048a34
DESKTOP-HJMTSJF\Zexelos <zexelosg@gmail.com>
2021-05-26 17:10:55 +0200

    added automapper

---
+++ src/EcommerceApp.Application/Mapping/IMapFrom.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace EcommerceApp.Application.Mapping
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile)
+        {
+            profile.CreateMap(typeof(T), GetType());
+        }
+    }
+}
