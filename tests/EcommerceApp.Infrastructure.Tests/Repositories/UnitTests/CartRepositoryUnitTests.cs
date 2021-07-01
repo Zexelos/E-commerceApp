a3141df298bfc396a6b54293f2867f4527aed13d
Adrian Polaczak <zexelosg@gmail.com>
2021-07-01 17:31:10 +0200

    added cart repository unit tests

--- tests/EcommerceApp.Infrastructure.Tests/Repositories/UnitTests/CartRepositoryUnitTests.cs
+++ tests/EcommerceApp.Infrastructure.Tests/Repositories/UnitTests/CartRepositoryUnitTests.cs
@@ -1,8 +1,59 @@
+using System.Collections.Generic;
 using System;
+using System.Threading.Tasks;
+using EcommerceApp.Domain.Models;
+using EcommerceApp.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using System.Linq;
 
 namespace EcommerceApp.Infrastructure.Tests.Repositories.UnitTests
 {
     public class CartRepositoryUnitTests
     {
+        private readonly DbContextOptions<AppDbContext> _options;
+
+        public CartRepositoryUnitTests()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+        }
+
+        [Fact]
+        public async Task AddCartAsync_AddsCart()
+        {
+            var cart = new Cart { Id = 100 };
+
+            using (var context = new AppDbContext(_options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var sut = new CartRepository(context);
+                await sut.AddCartAsync(cart);
+                var result = await context.Carts.FindAsync(cart.Id);
+                Assert.NotNull(result);
+                Assert.Equal(cart, result);
+            }
+        }
+
+        [Fact]
+        public async Task GetCarts_ReturnsIQueryableOfCarts()
+        {
+            var cart1 = new Cart { Id = 100 };
+            var cart2 = new Cart { Id = 150 };
+            var cart3 = new Cart { Id = 200 };
+            var carts = new List<Cart> { cart1, cart2, cart3 };
+            var cartsQ = carts.AsQueryable();
+
+            using (var context = new AppDbContext(_options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                await context.Carts.AddRangeAsync(carts);
+                await context.SaveChangesAsync();
+                var sut = new CartRepository(context);
+                var result = sut.GetCarts();
+                Assert.NotNull(result);
+                Assert.Equal(cartsQ, result);
+            }
+        }
     }
 }
