1cd3c3ffa1a2516a84b85e214e232c6abbc81bd1
Adrian Polaczak <zexelosg@gmail.com>
2021-07-02 01:53:33 +0200

    updated employee service unit tests

--- tests/EcommerceApp.Application.Tests/Services/UnitTests/EmployeeServiceUnitTests.cs
+++ tests/EcommerceApp.Application.Tests/Services/UnitTests/EmployeeServiceUnitTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Security.Claims;
 using System.Threading;
 using System.Linq;
@@ -13,26 +14,36 @@ using Moq;
 using Xunit;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
+using EcommerceApp.Application.Interfaces;
+using MockQueryable.Moq;
+using EcommerceApp.Application.ViewModels;
 
 namespace EcommerceApp.Application.Tests
 {
     public class EmployeeServiceUnitTests
     {
-        /*
+
         private readonly EmployeeService _sut;
         private readonly Mock<IMapper> _mapper = new();
         private readonly Mock<IEmployeeRepository> _employeeRepository = new();
         private readonly Mock<UserManager<AppUser>> _userManager;
+        private readonly Mock<IPasswordHasher<AppUser>> _passwordHasher = new();
+        private readonly Mock<IPaginatorService<EmployeeForListVM>> _paginatorService = new();
 
         public EmployeeServiceUnitTests()
         {
             var userStore = new Mock<IUserStore<AppUser>>();
             _userManager = new Mock<UserManager<AppUser>>(userStore.Object, null, null, null, null, null, null, null, null);
-            _sut = new EmployeeService(_mapper.Object, _employeeRepository.Object, _userManager.Object);
+            _sut = new EmployeeService(
+                _mapper.Object,
+                _employeeRepository.Object,
+                _userManager.Object,
+                _paginatorService.Object,
+                _passwordHasher.Object);
         }
 
         [Fact]
-        public async Task AddEmployee_MapperShouldReturnEmployee()
+        public async Task AddEmployeeAsync_MethodsRunOnce()
         {
             // Arrange
             var employeeVM = new EmployeeVM()
@@ -49,50 +60,62 @@ namespace EcommerceApp.Application.Tests
 
             _mapper.Setup(s => s.Map<Employee>(employeeVM)).Returns(employee);
 
+            _userManager.Setup(s => s.GenerateEmailConfirmationTokenAsync(It.IsAny<AppUser>())).ReturnsAsync("escg45egsv5egrs");
+
             // Act
             await _sut.AddEmployeeAsync(employeeVM);
 
             // Assert 
             _userManager.Verify(s => s.CreateAsync(It.IsAny<AppUser>(), It.IsAny<string>()), Times.Once);
-            _employeeRepository.Verify(s => s.AddEmplyeeAsync(It.IsAny<Employee>()), Times.Once);
             _userManager.Verify(s => s.AddClaimAsync(It.IsAny<AppUser>(), It.IsAny<Claim>()), Times.Once);
-            Assert.Equal(employee.FirstName, employeeVM.FirstName);
-            Assert.Equal(employee.LastName, employeeVM.LastName);
+            _userManager.Verify(s => s.ConfirmEmailAsync(It.IsAny<AppUser>(), It.IsAny<string>()));
         }
 
         [Fact]
-        public async Task GetEmployeeAsync_ReturnEMployeeVM()
+        public async Task GetEmployeeAsync_ReturnsEmployeeVM()
         {
             // Arrange
-            Employee employee = new()
+            var employee = new Employee
             {
                 Id = 10,
                 FirstName = "maciek",
-                LastName = "makowski"
+                LastName = "makowski",
+                AppUser = new AppUser
+                {
+                    Email = "maciek@masd.com"
+                }
             };
+            var employees = new List<Employee> { employee };
+            var employeesQ = employees.AsQueryable().BuildMock();
 
             EmployeeVM employeeVM = new()
             {
                 Id = 10,
                 FirstName = "maciek",
-                LastName = "makowski"
+                LastName = "makowski",
+                Email = "maciek@masd.com"
             };
 
-            _employeeRepository.Setup(s => s.GetEmployeeAsync(employee.Id)).ReturnsAsync(employee);
+            _employeeRepository.Setup(s => s.GetEmployees()).Returns(employeesQ.Object);
 
-            _mapper.Setup(s => s.Map<EmployeeVM>(employee)).Returns(employeeVM);
+            _mapper.Setup(x => x.ConfigurationProvider).Returns(
+                () => new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<Employee, EmployeeVM>().ForMember(x => x.Email, x => x.MapFrom(src => src.AppUser.Email));
+                }));
 
             // Act
             var result = await _sut.GetEmployeeAsync(employee.Id);
 
             // Assert
-            Assert.Equal(employee.Id, result.Id);
-            Assert.Equal(employee.FirstName, result.FirstName);
-            Assert.Equal(employee.LastName, result.LastName);
+            Assert.Equal(employeeVM.Id, result.Id);
+            Assert.Equal(employeeVM.FirstName, result.FirstName);
+            Assert.Equal(employeeVM.LastName, result.LastName);
+            Assert.Equal(employeeVM.Email, result.Email);
         }
 
         [Fact]
-        public async Task GetEmployeesAsync_ReturnListOfEmployeeVM()
+        public async Task GetPaginatedEmployeesAsync_ReturnsEmployeeListVM()
         {
             // Arrange
             List<Employee> employees = new()
@@ -100,27 +123,46 @@ namespace EcommerceApp.Application.Tests
                 new Employee() { FirstName = "maciek", LastName = "makowski" },
                 new Employee() { FirstName = "swe9guhv", LastName = "s34c63w4" },
             };
+            var employeesQ = employees.AsQueryable().BuildMock();
 
-            List<EmployeeVM> employeesVM = new()
+            List<EmployeeForListVM> employeesVM = new()
             {
-                new EmployeeVM() { FirstName = "maciek", LastName = "makowski" },
-                new EmployeeVM() { FirstName = "swe9guhv", LastName = "s34c63w4" },
+                new EmployeeForListVM() { FirstName = "maciek", LastName = "makowski" },
+                new EmployeeForListVM() { FirstName = "swe9guhv", LastName = "s34c63w4" },
             };
 
-            _employeeRepository.Setup(s => s.GetEmployeesAsync()).ReturnsAsync(employees.AsQueryable);
+            var paginatedVM = new PaginatedVM<EmployeeForListVM>
+            {
+                Items = employeesVM,
+                CurrentPage = 1,
+                TotalPages = 2,
+            };
 
-            _mapper.Setup(s => s.Map<List<EmployeeVM>>(employees)).Returns(employeesVM);
+            var employeeListVM = new EmployeeListVM
+            {
+                Employees = paginatedVM.Items,
+                CurrentPage = paginatedVM.CurrentPage,
+                TotalPages = paginatedVM.TotalPages
+            };
+
+            _employeeRepository.Setup(s => s.GetEmployees()).Returns(employeesQ.Object);
+
+            _mapper.Setup(x => x.ConfigurationProvider).Returns(
+                () => new MapperConfiguration(cfg => { cfg.CreateMap<Employee, EmployeeForListVM>(); }));
+
+            _paginatorService.Setup(x => x.CreateAsync(It.IsAny<IQueryable<EmployeeForListVM>>(), 1, 10)).ReturnsAsync(paginatedVM);
+
+            _mapper.Setup(x => x.Map<EmployeeListVM>(It.IsAny<PaginatedVM<EmployeeForListVM>>())).Returns(employeeListVM);
 
             // Act
-            var result = await _sut.GetEmployeesAsync();
+            var result = await _sut.GetPaginatedEmployeesAsync(10, 1);
 
             // Assert
-            Assert.Equal(result[0].FirstName, employeesVM[0].FirstName);
-            Assert.Equal(result[1].LastName, employeesVM[1].LastName);
+            Assert.Equal(employeeListVM, result);
         }
 
         [Fact]
-        public async Task UpdateEmployeeAsync_ShouldUpdateEEmployee()
+        public async Task UpdateEmployeeAsync_MethodsRunOnce()
         {
             // Arrange
             Employee employee = new()
@@ -128,89 +170,62 @@ namespace EcommerceApp.Application.Tests
                 Id = 10,
                 FirstName = "maciek",
                 LastName = "makowski",
-                AppUserId = "w23qrx2q3",
+                AppUser = new AppUser
+                {
+                    Email = "ziutek@askd.com",
+                    UserName = "ziutek",
+                    PasswordHash = "w34ct234ctq234ct"
+                }
             };
+            var employees = new List<Employee> { employee };
+            var employeesQ = employees.AsQueryable().BuildMock();
 
             EmployeeVM employeeVM = new()
             {
                 Id = 10,
                 FirstName = "maciek",
                 LastName = "makowski",
-                Password = "asfew"
+                Password = "asfew",
+                Email = "ziutek@askd.com"
             };
 
-            EmployeeVM employeeVM2 = new()
-            {
-                Id = 10,
-                FirstName = "maciek",
-                LastName = "makowski",
-                Password = null
-            };
+            _employeeRepository.Setup(s => s.GetEmployees()).Returns(employeesQ.Object);
 
-            AppUser user = new()
-            {
-                Id = "w23qrx2q3"
-            };
-
-            _employeeRepository.Setup(s => s.GetEmployeeAsync(employeeVM.Id)).ReturnsAsync(employee);
-
-            _userManager.Setup(s => s.FindByIdAsync(employee.AppUserId)).ReturnsAsync(new AppUser()
-            {
-                Id = "w23qrx2q3"
-            });
-
-            _mapper.Setup(s => s.Map<Employee>(employeeVM)).Returns(employee);
+            _passwordHasher.Setup(x => x.HashPassword(It.IsAny<AppUser>(), It.IsAny<string>())).Returns(employee.AppUser.PasswordHash);
 
             // Act
             await _sut.UpdateEmployeeAsync(employeeVM);
 
             // Assert
-            Assert.Equal(employee.Id, employeeVM.Id);
-            Assert.Equal(employee.AppUserId, user.Id);
-            _userManager.Verify(v => v.SetEmailAsync(It.IsAny<AppUser>(), It.IsAny<string>()), Times.Once);
-            _userManager.Verify(v => v.UpdateNormalizedEmailAsync(It.IsAny<AppUser>()), Times.Once);
-            _userManager.Verify(v => v.SetUserNameAsync(It.IsAny<AppUser>(), It.IsAny<string>()), Times.Once);
-            _userManager.Verify(v => v.UpdateNormalizedUserNameAsync(It.IsAny<AppUser>()), Times.Once);
-            _userManager.Verify(v => v.ResetPasswordAsync(It.IsAny<AppUser>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+            _employeeRepository.Verify(x => x.UpdateEmployeeAsync(It.IsAny<Employee>()), Times.Once);
         }
 
         [Fact]
-        public async Task CheckUpdateEmployee()
+        public async Task DeleteEmployeeAsync_MethodsRunOnce()
         {
             // Arrange
-            EmployeeVM employeeVM = new()
-            {
-                Id = 10,
-                FirstName = "maciek",
-                LastName = "makowski",
-                Password = "asfew"
-            };
-
             Employee employee = new()
             {
                 Id = 10,
                 FirstName = "maciek",
                 LastName = "makowski",
-                AppUserId = "w23qrx2q3",
+                AppUser = new AppUser
+                {
+                    Email = "ziutek@askd.com",
+                    UserName = "ziutek",
+                    PasswordHash = "w34ct234ctq234ct"
+                }
             };
+            var employees = new List<Employee> { employee };
+            var employeesQ = employees.AsQueryable().BuildMock();
 
-            AppUser user = new()
-            {
-                Id = "w23qrx2q3"
-            };
-
-            _employeeRepository.Setup(s => s.GetEmployeeAsync(employeeVM.Id)).ReturnsAsync(employee);
-
-            _userManager.Setup(s => s.FindByIdAsync(employee.AppUserId)).ReturnsAsync(user);
+            _employeeRepository.Setup(s => s.GetEmployees()).Returns(employeesQ.Object);
 
             // Act
-            await _sut.DeleteEmployeeAsync(employeeVM.Id);
+            await _sut.DeleteEmployeeAsync(employee.Id);
 
             // Assert
-            Assert.Equal(user.Id, employee.AppUserId);
-            _userManager.Verify(v => v.DeleteAsync(It.IsAny<AppUser>()), Times.Once);
-            //_employeeRepository.Verify(v => v.DeleteEmployeeAsync(It.IsAny<int>()), Times.Once);
+            _userManager.Verify(x => x.DeleteAsync(It.IsAny<AppUser>()), Times.Once);
         }
-        */
     }
 }
